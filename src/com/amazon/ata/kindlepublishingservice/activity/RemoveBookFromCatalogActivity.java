7ea89070b3fc7a4998686ae01959610fff7f514f
Aidan Au <aidan.au.linkedin@gmail.com>
2024-02-03 16:51:30 -0500

    MT1 completed class diagram

--- src/com/amazon/ata/kindlepublishingservice/activity/RemoveBookFromCatalogActivity.java
+++ src/com/amazon/ata/kindlepublishingservice/activity/RemoveBookFromCatalogActivity.java
@@ -1,5 +1,6 @@
 package com.amazon.ata.kindlepublishingservice.activity;
 
+import com.amazon.ata.kindlepublishingservice.dao.CatalogDao;
 import com.amazon.ata.kindlepublishingservice.models.requests.RemoveBookFromCatalogRequest;
 import com.amazon.ata.kindlepublishingservice.models.response.RemoveBookFromCatalogResponse;
 import com.amazonaws.services.lambda.runtime.Context;
@@ -7,7 +8,9 @@ import com.amazonaws.services.lambda.runtime.Context;
 import javax.inject.Inject;
 
 public class RemoveBookFromCatalogActivity {
+    private CatalogDao catalogDao; //CatalogDao to access the Catalog table
     @Inject
+    // may have to put CatalogDao as a parameter
     RemoveBookFromCatalogActivity() {}
     public RemoveBookFromCatalogResponse execute(RemoveBookFromCatalogRequest removeBookFromCatalogRequest) {
         return null;
